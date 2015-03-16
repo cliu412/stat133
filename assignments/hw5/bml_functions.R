6caf29375678fb452cb314aa3c180f97e541d6a5
Christopher Liu <cliu412@berkeley.edu>
2015-03-15 18:51:36 -0700

    HW5

--- assignments/hw5/bml_functions.R
+++ assignments/hw5/bml_functions.R
@@ -22,14 +22,33 @@ m<-matrix(data=sample(0:2,r*c,replace=TRUE,prob=c(1-p,p/2,p/2)),nrow=r,ncol=c)
 
 bml.step <- function(m){
 
-  
-   #return(list(m, grid.new))
-}
+  mstar<-matrix(c(m[,2:ncol(m)],m[,1]),nrow=nrow(m),ncol=ncol(m))
+  stayingput.red<-2*(m==2)+1*(m==1&mstar!=0)
+  movers.red<-1*(m==1&mstar==0)
+  movers.red<-movers.red[,c(ncol(m),1:(ncol(m)-1))]
+  mnew.red<-stayingput.red+movers.red
+  mmoon<-mnew.red[c(nrow(mnew.red),1:(nrow(mnew.red)-1)),]
+  stayingput.blue<-1*(mnew.red==1)+2*(mnew.red==2&mmoon!=0)
+  movers.blue<-2*(m==2&mmoon==0)
+  movers.blue<-movers.blue[c(2:nrow(m),1),]
+  mnew.red.blue<-stayingput.blue+movers.blue
+  return(mnew.red.blue)
 
+}
 #### Function to do a simulation for a given set of input parameters
 ## Input : size of grid [r and c] and density [p]
 ## Output : *up to you* (e.g. number of steps taken, did you hit gridlock, ...)
 
 bml.sim <- function(r, c, p){
-
+  
+  m=bml.init(r,c,p)
+  x=(bml.step(m))
+  for (i in 1:1000){
+    n=x
+    x=bml.step(x)
+    if (all.equal(n,x)==TRUE){
+      return (paste("GRIDLOCK!! at traffic step",i))
+    }
+  } 
+  return ("Traffic went 1000 steps with no GRIDLOCK!")
 }
--- assignments/hw5/bml_simulation.R
+++ assignments/hw5/bml_simulation.R
@@ -7,4 +7,24 @@
 #### The output can e.g. be how many steps the system took until it hit gridlock or
 #### how many steps you observered before concluding that it is in a free flowing state.
 
-
+bml.run<-function (r,c,p,d,n){
+  
+  results=c()
+  for (q in 1:d){
+    m=bml.init(r,c,p)
+    x=(bml.step(m))
+    for (i in 1:n){
+      f=x
+      x=bml.step(x)
+      if (all.equal(f,x)==TRUE){
+        results=c(results,i)
+        break
+     }
+      if (i==n){
+        results=c(results,"Free Flow")
+      }
+    }
+  }
+  return (list(c(results)))
+  return ()
+}

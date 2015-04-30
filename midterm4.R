0626ab3decf6adfeac6f4d6e2c1e39447bdcf43e
Christopher Liu <cliu412@berkeley.edu>
2015-04-30 15:20:43 -0700

    midterm 4

--- midterm4.R
+++ midterm4.R
@@ -20,7 +20,13 @@ set.seed(123456)
 # We've set the default inputs to k=2 and B=100
 
 dice_sum <- function(k=2, B=100){
-
+  
+  dice.rolls <- c(1,2,3,4,5,6)
+  results <- c()
+  for (i in 1:B){
+    results <- (c(sum(sample(dice.rolls,k,replace=TRUE))))
+  }
+  return (results)
 }
 
 #### String manipulation
@@ -29,20 +35,20 @@ phrases <- c("dog", "doggy", "den", "good boy", "Really?", "How much?", "Only $8
 
 # Create a vector [text1] that lists the elements in phrases 
 # where the SECOND TO LAST character is "o" (lower case o).
-#text1 <- <your code here>
+text1 <- grep("o${1}\\>",phrases,value=TRUE)
 
 # Create a vector [text2] that lists the elements in phrases that
 # START with the letter "d"
-#text2 <- <your code here>
+text2 <- grep("\\<d",phrases,value=TRUE)
 
 # Create a variable [no.punct] that equals the number of phrases with a punctuation mark in it.
-# no.punct <- <your code here>
+no.punct <- length(grep("?[[:punct:]]",phrases))
 
 # Create a vector [even] that is of length 1000 and has the entries
 # "even2", "even4", ...
 # with no separation between the word and the letter
 
-#even <- <your code here>
+even <- paste("even",seq(from=2, to=2000, by=2),sep="")
 
 
 # Start with [hotelCal] which is a character string, create 
@@ -53,7 +59,8 @@ phrases <- c("dog", "doggy", "den", "good boy", "Really?", "How much?", "Only $8
 
 hotelCal <- "On a dark desert highway, cool wind in my hair. Warm smell of colitas, rising up through the air. Up ahead in the distance, I saw a shimmering light. My head grew heavy and my sight grew dim I had to stop for the night.  There she stood in the doorway; I heard the mission bell.  And I was thinking to myself: 'This could be heaven or this could be hell'. Then she lit up a candle and she showed me the way."
 
-# hotelCal.split <- <your code here> 
+hotelCal.split <-  tolower(hotelCal)
+hotelCal.split <- gsub("[:punct:]","",hotelCal.split)
 
 
 # Write a function called updateDate. Your function should take the following
@@ -68,7 +75,9 @@ hotelCal <- "On a dark desert highway, cool wind in my hair. Warm smell of colit
 #     updated. For example updateDate(c('May, 2008', 'June, 2011'), '2008') should
 #     return 'May, 2015'.
 updateDate <- function(dates, old.yr) {
-
+  indices <- grep(old.yr, dates)
+  new.dates <- gsub(old.yr,2015,dates)
+  return (new.dates[indices])
 }
 
 
